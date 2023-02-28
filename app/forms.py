bbc9ff58847d02e29d4c684a6895df3761647802
Fredrick Dave Santulan <(none)>
2023-02-28 09:59:44 +0800

    Remove duplicate code

--- app/forms.py
+++ app/forms.py
@@ -39,10 +39,6 @@ class EditProfileForm(FlaskForm):
         super().__init__(*args, **kwargs)
         self.original_username = original_username
 
-    # def __init__(self, original_username, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.original_username = original_username
-
     def validate_username(self, username):
         if username.data != self.original_username:
             user = User.query.filter_by(username=self.username.data).first()
