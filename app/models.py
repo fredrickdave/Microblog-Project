3838640c6e0d49bfdd72a45c0f784789ea2e3685
Fredrick Dave Santulan <(none)>
2023-03-01 10:09:08 +0800

    Add many to many db relationship

--- app/models.py
+++ app/models.py
@@ -6,6 +6,12 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from app import db, login_manager
 
+followers = db.Table(
+    "followers",
+    db.Column("follower_id", db.Integer, db.ForeignKey("user.id")),
+    db.Column("followed_id", db.Integer, db.ForeignKey("user.id")),
+)
+
 
 class User(db.Model, UserMixin):
     __tablename__ = "user"
@@ -16,6 +22,14 @@ class User(db.Model, UserMixin):
     posts = db.relationship("Post", backref="author", lazy="dynamic")
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        "User",
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref("followers", lazy="dynamic"),
+        lazy="dynamic",
+    )
 
     def __repr__(self):
         return "<User {}>".format(self.username)
