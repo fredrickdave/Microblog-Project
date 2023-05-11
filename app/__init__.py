0c6f78ea18bd1f298d50501b3d751fbf96f62bda
Fredrick Dave Santulan <(none)>
2023-05-11 10:02:48 +0800

    Add config choice argument for create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -19,9 +19,9 @@ ckeditor = CKEditor()
 moment = Moment()
 
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
