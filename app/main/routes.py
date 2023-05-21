bbc7575dc9312a2e274934f66b038e357521dc56
Fredrick Dave Santulan <(none)>
2023-05-21 12:52:29 +0800

    Add search view function

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 
-from flask import current_app, flash, redirect, render_template, request, url_for
+from flask import current_app, flash, g, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 
 from app import db
 from app.main import bp
-from app.main.forms import CreatePostForm, EditProfileForm, EmptyForm
+from app.main.forms import CreatePostForm, EditProfileForm, EmptyForm, SearchForm
 from app.models import Post, User
 
 
@@ -19,6 +19,7 @@ def before_request():
         if time_difference > 10:
             current_user.last_seen = datetime.utcnow()
             db.session.commit()
+        g.search_form = SearchForm()
 
 
 @bp.route("/")
@@ -125,3 +126,19 @@ def new_post():
         flash("Your post is now live!")
         return redirect(url_for("main.index"))
     return render_template("make_post.html", form=form)
+
+
+@bp.route("/search")
+@login_required
+def search():
+    if not g.search_form.validate():
+        return redirect(url_for("main.explore"))
+    page = request.args.get("page", 1, type=int)
+    posts, total = Post.search(g.search_form.q.data, page, current_app.config["POSTS_PER_PAGE"])
+    next_url = (
+        url_for("main.search", q=g.search_form.q.data, page=page + 1)
+        if total > page * current_app.config["POSTS_PER_PAGE"]
+        else None
+    )
+    prev_url = url_for("main.search", q=g.search_form.q.data, page=page - 1) if page > 1 else None
+    return render_template("search.html", title="Search", posts=posts, next_url=next_url, prev_url=prev_url)
