741adbf98f42f898ddfb4272a0691925d97e9740
Fredrick Dave Santulan <(none)>
2023-05-21 13:41:58 +0800

    Increase ES result to 999 entries

--- app/search.py
+++ app/search.py
@@ -26,8 +26,7 @@ def query_index(index, query, page, per_page):
         index=index,
         body={
             "query": {"multi_match": {"query": query, "fields": ["*"]}},
-            "from": (page - 1) * per_page,
-            "size": per_page,
+            "size": 999,
         },
     )
     ids = [int(hit["_id"]) for hit in search["hits"]["hits"]]
